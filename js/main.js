6f020d816684f50e2eb3c271a0e77a152da2ac0e
Suheyb <84540307+byehus@users.noreply.github.com>
2025-09-02 22:30:32 +0100

    JSInfo Tasks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+// Variables tasks
 
+// Task 1 - Working with variables
+
+// let name, admin;
+
+// name = "John";
+
+// admin = name;
+
+// alert(admin);
+
+// Task 2 - Giving the right name
+
+// const ourPlanet = "Earth";
+
+// let currentUserName = "John";
+
+// alert(ourPlanet);
+
+// alert(currentVisitor);
+
+// Task 3 - Uppercase const?
+
+// const BIRTHDAY = '18.04.1982'; // this should be uppercase since birthday does not change
+
+// const age = someCode(birthday);
